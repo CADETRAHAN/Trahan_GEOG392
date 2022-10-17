f025fd38ea83c3443eb3168f01cfa1bf5e2f8743
CADETRAHAN <cadetrahan@tamu.edu>
2022-10-17 11:04:44 -0500

    update lab04

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -1,4 +1,4 @@
 import arcpy
-arcpy.env.workspace = "C:/Users/cadet/DevSource/lab04"
+arcpy.env.workspace = "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04"
 arcpy.CreateFileGDB_management(arcpy.env.workspace, "lab04.gdb")
-garages = arcpy.management.MakeXYEventLayer("garages.csv", "LotName", "x", "y", "wh")
+garages = arcpy.management.MakeXYEventLayer("garages.csv", "x", "y",)
