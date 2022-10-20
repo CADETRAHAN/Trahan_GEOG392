323ea55a9e06abaaca5266eab9c9e44ca261a28d
CADETRAHAN <cadetrahan@tamu.edu>
2022-10-20 09:58:02 -0500

    Update Lab04

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -1,8 +1,28 @@
 import arcpy
-arcpy.env.workspace = "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04"
-garages = arcpy.management.MakeXYEventLayer("garages.csv", "x", "y", "garages")
-gands = ["Structures.shp", garages]
-arcpy.FeatureClassToGeodatabase_conversion(garages, "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/lab04.gdb")
-return2 = arcpy.Buffer_analysis(garages, "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/Buffgarages", 150)
-Intersect = arcpy.Intersect_analysis(return2 + "Structures.shp", Intersect, ALL) 
-arcpy.TableToTable_conversion(Intersect, "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04", finaltable)
+
+def __main__():
+    arcpy.env.workspace = "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04"
+    # arcpy.CreateFileGDB_management("C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/", "DB")
+    CSV = "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/garages.csv"
+    RIPX = "X"
+    RIPY = "Y"
+    lol = "garage_points"
+    return1 = arcpy.MakeXYEventLayer_management(CSV, RIPX, RIPY, lol)
+
+    garages = "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/garage_points"
+    duh = r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb"
+    arcpy.FeatureClassToGeodatabase_conversion(return1, duh)
+
+    swag= r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/Campus.gdb/Structures"
+    awesome= r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/Structures"
+    arcpy.Copy_management(swag, awesome)
+
+    
+    spatial_ref = arcpy.Describe(awesome).spatialReference
+    arcpy.Project_management(garages, "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/ProjectGarages", spatial_ref)
+
+
+
+
+if __name__ == '__main__':
+    __main__()
