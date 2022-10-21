5670da2f4e780737d917deb18262e73664b09118
CADETRAHAN <cadetrahan@tamu.edu>
2022-10-20 19:38:16 -0500

    Update Lab04

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -3,26 +3,40 @@ import arcpy
 def __main__():
     arcpy.env.workspace = "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04"
     # arcpy.CreateFileGDB_management("C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/", "DB")
-    CSV = "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/garages.csv"
-    RIPX = "X"
-    RIPY = "Y"
-    lol = "garage_points"
-    return1 = arcpy.MakeXYEventLayer_management(CSV, RIPX, RIPY, lol)
+        #Creation of the Workspace and Database
 
-    garages = "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/garage_points"
-    duh = r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb"
-    arcpy.FeatureClassToGeodatabase_conversion(return1, duh)
+        #Creation of XY layer from CSV
+    CSV = r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/garages.csv"
+    coordinate1 = "X"
+    coordinate2 = "Y"
+    garageout = "garage_points"
+    return1 = arcpy.MakeXYEventLayer_management(CSV, coordinate1,coordinate2, garageout)
 
-    swag= r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/Campus.gdb/Structures"
-    awesome= r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/Structures"
-    arcpy.Copy_management(swag, awesome)
+        #Inputting the new XY Layer to the Database
+    garages = r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/garage_points"
+    database = r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb"
+    arcpy.FeatureClassToGeodatabase_conversion(return1, database)
 
-    
-    spatial_ref = arcpy.Describe(awesome).spatialReference
-    arcpy.Project_management(garages, "C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/ProjectGarages", spatial_ref)
+        #Inputting the Structure Shapefile from the Class Database to the Created Database
+    StructuresOld= r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/Campus.gdb/Structures"
+    StructuresNew= r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/Structures"
+    arcpy.Copy_management(StructuresOld, StructuresOld)
 
+        #The Spatial Reference from the Structures File being appended to the new XY file
+    spatial_ref = arcpy.Describe(StructuresOld).spatialReference
+    arcpy.Project_management(garages, r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/ProjectGarages", spatial_ref)
 
+        #150m Buffer of the XY file
+    projected = r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/ProjectGarages"
+    BufferLoc = r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/BufferGarages"
+    return2 = arcpy.Buffer_analysis(projected, BufferLoc, 150)
 
+        #Intersect of the Buffer and Structures Shapefiles
+    IntersectLoc = r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04/DB.gdb/IntersectBufferStructures"
+    arcpy.Intersect_analysis([BufferLoc, awesome],IntersectLoc, "ALL")
+
+        #Output the Intersect as a new Table
+    arcpy.TableToTable_conversion(IntersectLoc, r"C:/Users/cadet/DevSource/Trahan_GEOG392/lab/lab04", r"lab04.csv")
 
 if __name__ == '__main__':
     __main__()
