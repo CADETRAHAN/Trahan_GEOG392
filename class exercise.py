8ec8b8d716fc59390854689e31538018a4e5b671
CADETRAHAN <cadetrahan@tamu.edu>
2022-11-08 20:56:13 -0600

    lab05 commit

--- class exercise.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-#print(Path.cwd())
-
-path = 'C:/Users/cadet/Devsource/Trahan_GEOG392/geog392.txt'
-fileread = open(path, 'r')
-myread = fileread.readlines()
-for line in myread:
-    print(line)
-fileread.close()
