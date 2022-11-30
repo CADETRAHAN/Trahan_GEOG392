207205495182c5f05698a4e9e67cc963ba089473
CADETRAHAN <cadetrahan@tamu.edu>
2022-11-30 16:38:58 -0600

    push lab03

--- lab/lab03/lab03.py
+++ lab/lab03/lab03.py
@@ -1,15 +1,37 @@
+import math
 from pathlib import Path
 print(Path.cwd())
-path = "C:\Users\cadet\DevSource\Trahan_GEOG392\lab\lab03\lab03.txt"
 
-file = open(path, "r")
+def read_file(file):
+    path = r"C:\Users\cadet\DevSource\Trahan_GEOG392\lab\lab03\lab03.txt"
+    file = open(path, "r")
+    lines =file.readlines()
+    file.close
+    
+    for line in lines:
+        components = line.strip().split()
+        Shape = components[0]
+        print = Shape
 
-class Rectangle(Shape):
-    def __init__(self, l, w):
-        self.l = l
-        self.w = w
-    def getArea(self):
-        return self.l * self.w
 
+#class Circle(Shape):
+#    def __init__(self,r):
+#        self.r = r
+#    def getArea(self):
+#        return self.r * self.r * math.pi
 
-def read_file(file)
+#class Triangle(Shape):
+#    def __init__(self,l,w):
+#        self.l = l
+#        self.w = w
+#    def getArea(self):
+#        return ((self.l * self.w) / 2)
+
+#class Rectangle(Shape):
+#    def __init__(self, l, w):
+#        self.l = l
+#        self.w = w
+#    def getArea(self):
+#        return self.l * self.w
+
+print(read_file)
