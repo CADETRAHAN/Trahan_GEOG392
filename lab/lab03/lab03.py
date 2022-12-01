a69bb6d86bffce2e178e2d4ae146b0ee582a0036
CADETRAHAN <cadetrahan@tamu.edu>
2022-12-01 00:29:08 -0600

    push Oil Field

--- lab/lab03/lab03.py
+++ lab/lab03/lab03.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 print(Path.cwd())
 path = "C:\Users\cadet\DevSource\Trahan_GEOG392\lab\lab03\lab03.txt"
@@ -11,5 +12,17 @@ class Rectangle(Shape):
     def getArea(self):
         return self.l * self.w
 
+class Triangle(Shape):
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+    def getArea(self):
+        return self.l * self.w / 2
+
+class Circle(Shape):
+    def __init__(self, d)
+        self.r = r
+    def getArea(self):
+        return self.r * self.r * math.pi
 
 def read_file(file)
