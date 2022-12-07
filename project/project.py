c22c1129e4bcf3db49bd0a12c43094082aa73c1b
CADETRAHAN <112340616+CADETRAHAN@users.noreply.github.com>
2022-12-07 16:03:36 -0600

    Update project.py

--- project/project.py
+++ project/project.py
@@ -1,7 +1,35 @@
 import arcpy
+import os
+from arcpy import env
+from arcpy.sa import *
+arcpy.env.overwriteOutput = True
 
-def __main__():
-    arcpy.env.workspace = "C:\Users\cadet\DevSource\Trahan_GEOG392\project\Oil-Field Convertability"
+#Data Input and Output
+Well_Input = arcpy.GetParameter(0)
+Power_Line_Input = arcpy.GetParameter(1)
+Irradiance_Raster = arcpy.GetParameter(2)
+Profit_Wells = arcpy.GetParameter(3)
 
-if __name__ == '__main__':
-    __main__()
+#Mathmatic Expressions for Profit Calculation
+Expression = "!GROSS_PROFIT! - !TOTAL_COST!"
+Meters2Miles = "(!NEAR_DIST!/1609.34)"
+Spur_Cost = "(!DIST_MILES!*847000)"
+Total_Cost = "(1481000 + !SPUR_COST!)"
+Zero_Distance_Profit = "(!RASTERVALU! * 524464.2)"
+
+#Finding the Irradiance per Well Site and making a new Feature Class
+try:
+    ExtractValuesToPoints(Well_Input, Irradiance_Raster, Profit_Wells)
+except: 
+    print(arcpy.GetMessages())
+
+#Mathmatics of Cost Including Finding the Nearest Distance from a Well to a Power Line
+try:
+    arcpy.Near_analysis(Profit_Wells, Power_Line_Input, "Miles", "NO_LOCATION", "NO_ANGLE", "GEODESIC")
+    arcpy.CalculateField_management(Profit_Wells, "DIST_MILES", Meters2Miles, None, None, "FLOAT")
+    arcpy.CalculateField_management(Profit_Wells, "SPUR_COST", Spur_Cost, None, None, "FLOAT")
+    arcpy.CalculateField_management(Profit_Wells, "TOTAL_COST", Total_Cost, None, None, "FLOAT")
+    arcpy.CalculateField_management(Profit_Wells, "GROSS_PROFIT", Zero_Distance_Profit, None, None, "FLOAT")
+    arcpy.CalculateField_management(Profit_Wells, "Net_Profit", Expression, None, None, "FLOAT")
+except:
+    print(arcpy.GetMessages())
