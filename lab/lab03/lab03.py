8d88251005a382d0b1efdb0f5037274713642c63
CADETRAHAN <cadetrahan@tamu.edu>
2022-12-09 20:32:06 -0600

    push Project

--- lab/lab03/lab03.py
+++ lab/lab03/lab03.py
@@ -1,48 +1,36 @@
 import math
 from pathlib import Path
-print(Path.cwd())
+
+path = r"C:\Users\cadet\Documents\GitHub\Trahan_GEOG392\lab\lab03\lab03.txt"
+file = open(path, "r")
 
 def read_file(file):
-    path = r"C:\Users\cadet\DevSource\Trahan_GEOG392\lab\lab03\lab03.txt"
+    path = r"C:\Users\cadet\Documents\GitHub\Trahan_GEOG392\lab\lab03\lab03.txt"
     file = open(path, "r")
-    lines =file.readlines()
+    lines = file.readlines()
     file.close
-    
+
     for line in lines:
-        components = line.split(",")
+        components = line.strip().split(",")
         Shape = components[0]
 
-class Triangle(Shape):
-    def __init__(self, l, w):
-        self.l = l
-        self.w = w
-    def getArea(self):
-        return self.l * self.w / 2
+    class Triangle(Shape):
+        def __init__(self, l, w):
+            self.l = l
+            self.w = w
+        def getArea(self):
+            return self.l * self.w / 2
 
-class Circle(Shape):
-    def __init__(self, d)
-        self.r = r
-    def getArea(self):
-        return self.r * self.r * math.pi
+    class Circle(Shape):
+        def __init__(self, l):
+            self.l = l
+        def getArea(self):
+            return self.l * self.l * math.pi
 
-#class Circle(Shape):
-#    def __init__(self,r):
-#        self.r = r
-#    def getArea(self):
-#        return self.r * self.r * math.pi
-
-#class Triangle(Shape):
-#    def __init__(self,l,w):
-#        self.l = l
-#        self.w = w
-#    def getArea(self):
-#        return ((self.l * self.w) / 2)
-
-#class Rectangle(Shape):
-#    def __init__(self, l, w):
-#        self.l = l
-#        self.w = w
-#    def getArea(self):
-#        return self.l * self.w
-
-print(read_file)
+    class Rectangle(Shape):
+        def __init__(self,l,w):
+            self.l = l
+            self.w = w
+        def getArea(self):
+            return self.l * self.w
+read_file(file)
